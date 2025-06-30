3365b91643234842879d0c0eb09765fd1937d50a
jje <jejeong@win.co.kr>
2025-06-30 13:10:44 +0900

    feat : highlight current route in navigation

--- src/components/cards/CategorieCard.jsx
+++ src/components/cards/CategorieCard.jsx
@@ -1,6 +1,8 @@
+import { useLocation } from "react-router-dom";
 import { buttons, text, inputs, cards, toggles } from "../data/ComponentsData";
 
-function CategorieCard({ type = "", onClick }) {
+function CategorieCard({ type = "", path, onClick }) {
+  const location = useLocation();
   let count = 0;
 
   if (type.toLowerCase() === "buttons") {
@@ -14,6 +16,11 @@ function CategorieCard({ type = "", onClick }) {
   } else if (type.toLowerCase() === "toggles") {
     count = toggles.length;
   }
+
+  // check if crrent route matches the menu
+  const isActive = location.pathname === path;
+  console.log(isActive);
+
   return (
     <ul>
       <li className="mb-3">
@@ -23,11 +30,23 @@ function CategorieCard({ type = "", onClick }) {
           className="group flex w-full cursor-pointer items-center justify-between"
         >
           <span className="flex items-center">
-            <span className="text-base font-medium text-gray-900 group-hover:text-blue-700 dark:text-white dark:group-hover:text-blue-600">
+            <span
+              className={`text-base font-medium ${
+                isActive
+                  ? "text-blue-700 dark:text-blue-600"
+                  : "text-gray-900 group-hover:text-blue-700 dark:text-white dark:group-hover:text-blue-600"
+              }`}
+            >
               {type}
             </span>
           </span>
-          <span className="text-base font-medium text-gray-500 group-hover:text-blue-700 dark:text-gray-400 dark:group-hover:text-blue-600">
+          <span
+            className={`text-base font-medium ${
+              isActive
+                ? "text-blue-700 dark:text-blue-600"
+                : "text-gray-500 group-hover:text-blue-700 dark:text-gray-400 dark:group-hover:text-blue-600"
+            } `}
+          >
             ({`${count}`})
           </span>
         </button>
--- src/components/nav/CategoriesSideBar.jsx
+++ src/components/nav/CategoriesSideBar.jsx
@@ -14,6 +14,14 @@ function CategoriesSideBar() {
     "group inline-flex cursor-pointer items-center justify-center border gap-1 border-gray-600 bg-gray-700 p-2 text-sm font-medium text-gray-100 hover:bg-gray-900 hover:text-blue-600 focus:z-10 focus:ring-2 focus:ring-blue-500";
   const activeClass = "text-blue-600 ring-2 ring-blue-500 bg-gray-900";
 
+  const menuItems = [
+    { id: "texts", type: "Text", path: "/" },
+    { id: "buttons", type: "Buttons", path: "/buttons" },
+    { id: "inputs", type: "Input Field", path: "/inputs" },
+    { id: "cards", type: "Cards", path: "/cards" },
+    { id: "toggles", type: "Toggles", path: "/toggles" },
+  ];
+
   return (
     <aside className="hide-scrollbar col-span-1 hidden h-[calc(100vh-100px)] max-h-[100dvh] max-w-[250px] flex-1 overflow-y-hidden rounded-lg border border-gray-700 bg-gray-800 p-5 sm:flex">
       <div className="hidden h-full grid-rows-[auto_auto_auto_auto] sm:grid">
@@ -89,61 +97,14 @@ function CategoriesSideBar() {
           <h2 className="mb-4 text-sm font-medium text-gray-500 uppercase dark:text-gray-400">
             Components
           </h2>
-          <CategorieCard onClick={() => navigate("/")} type="Text" />
-          <CategorieCard onClick={() => navigate("/buttons")} type="Buttons" />
-          <CategorieCard
-            onClick={() => navigate("/inputs")}
-            type="Input Field"
-          />
-          <CategorieCard onClick={() => navigate("/cards")} type="Cards" />
-          <CategorieCard onClick={() => navigate("/toggles")} type="Toggles" />
-          <CategorieCard onClick={() => navigate("/")} type="Text" />
-          <CategorieCard onClick={() => navigate("/buttons")} type="Buttons" />
-          <CategorieCard
-            onClick={() => navigate("/inputs")}
-            type="Input Field"
-          />
-          <CategorieCard onClick={() => navigate("/cards")} type="Cards" />
-          <CategorieCard onClick={() => navigate("/")} type="Text" />
-          <CategorieCard onClick={() => navigate("/buttons")} type="Buttons" />
-          <CategorieCard
-            onClick={() => navigate("/inputs")}
-            type="Input Field"
-          />
-          <CategorieCard onClick={() => navigate("/cards")} type="Cards" />
-          <CategorieCard type="Toggle" />
-          <CategorieCard onClick={() => navigate("/")} type="Text" />
-          <CategorieCard onClick={() => navigate("/buttons")} type="Buttons" />
-          <CategorieCard
-            onClick={() => navigate("/inputs")}
-            type="Input Field"
-          />
-          <CategorieCard onClick={() => navigate("/cards")} type="Cards" />
-          <CategorieCard type="Toggle" />
-          <CategorieCard onClick={() => navigate("/")} type="Text" />
-          <CategorieCard onClick={() => navigate("/buttons")} type="Buttons" />
-          <CategorieCard
-            onClick={() => navigate("/inputs")}
-            type="Input Field"
-          />
-          <CategorieCard onClick={() => navigate("/cards")} type="Cards" />
-          <CategorieCard type="Toggle" />
-          <CategorieCard onClick={() => navigate("/")} type="Text" />
-          <CategorieCard onClick={() => navigate("/buttons")} type="Buttons" />
-          <CategorieCard
-            onClick={() => navigate("/inputs")}
-            type="Input Field"
-          />
-          <CategorieCard onClick={() => navigate("/cards")} type="Cards" />
-          <CategorieCard type="Toggle" />
-          <CategorieCard onClick={() => navigate("/")} type="Text" />
-          <CategorieCard onClick={() => navigate("/buttons")} type="Buttons" />
-          <CategorieCard
-            onClick={() => navigate("/inputs")}
-            type="Input Field"
-          />
-          <CategorieCard onClick={() => navigate("/cards")} type="Cards" />
-          <CategorieCard type="Toggle" />
+          {menuItems.map((menu) => (
+            <CategorieCard
+              key={menu.id}
+              onClick={() => navigate(menu.path)}
+              type={menu.type}
+              path={menu.path}
+            />
+          ))}
         </div>
 
         {/* FOOTER */}
