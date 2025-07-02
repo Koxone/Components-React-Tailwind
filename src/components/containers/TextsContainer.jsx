499c628e9992d5dec4b367099f7453ccde145276
jje <jejeong@win.co.kr>
2025-07-02 16:06:53 +0900

    refactor: change index to unique id in component arrays

--- src/components/containers/TextsContainer.jsx
+++ src/components/containers/TextsContainer.jsx
@@ -4,9 +4,9 @@ import { text } from "../data/ComponentsData";
 function CardsContainer() {
   return (
     <div className="hide-scrollbar grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-      {text.map(({ title, Component }, index) => (
+      {text.map(({ title, Component, id }) => (
         <MainCard
-          key={index}
+          key={id}
           title={title}
           effect={<Component>Sample Text</Component>}
           url={`/demo/${encodeURIComponent(title.toLowerCase().replace(/\s+/g, "-"))}`}
